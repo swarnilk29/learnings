a35b6911198d27e3a988a0062ce8a980b6f5a8f7
Swarnil Kokulwar <132215636+swarnilk29@users.noreply.github.com>
2025-12-27 12:21:37 +0530

    Add method to calculate total price of books

--- langs/springboot/3_SB_MVC_Library/src/main/java/com/example/main/repository/BookRepository.java
+++ langs/springboot/3_SB_MVC_Library/src/main/java/com/example/main/repository/BookRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.main.model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
-
+  @Query("select sum(b.price) from Book b")
+    Double getTotalPrice();
 }
